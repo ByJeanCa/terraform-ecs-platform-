829752f5b8afca7ecd4748efe09f4f5fab15e4d0
byjeanca <jo7624822@gmail.com>
2025-10-17 14:12:46 -0600

    feat: ACM module

---
+++ modules/acm/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 6.0.0"
+    }
+  }
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name = var.domain
+  validation_method = "DNS"
+
+  tags = var.common_tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
